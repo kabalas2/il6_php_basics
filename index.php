6d8b7fbd19a4115bb7eb8a4f85b0e4a98f73b04c
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-01-05 18:30:13 +0200

    kazkurios pamokos

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <?php
+include 'functions.php';
+// $string = 'Hello World';
 
-$string = 'Hello World';
+// $productName = 'Rudeniniai Batai Rudi';
+// $productDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt tincidunt nunc nec vulputate. Etiam imperdiet quis erat at iaculis. Sed ac ipsum ultrices justo mattis maximus. Vivamus convallis fringilla ex at pulvinar. Aliquam eu dolor eu nunc tristique auctor. Suspendisse potenti. Suspendisse potenti.';
+// $productBrand = 'Nike';
+// $symbol = '@';
+// $number = '1';
 
-$productName = 'Rudeniniai Batai Rudi';
-$productDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt tincidunt nunc nec vulputate. Etiam imperdiet quis erat at iaculis. Sed ac ipsum ultrices justo mattis maximus. Vivamus convallis fringilla ex at pulvinar. Aliquam eu dolor eu nunc tristique auctor. Suspendisse potenti. Suspendisse potenti.';
-$productBrand = 'Nike';
-$symbol = '@';
-$number = '1';
-
-$products = null;
+// $products = null;
 
 
 
-$productQty = 4;
-$holesCount = 32;
-$productsManufacturerYears = 2021;
+// $productQty = 4;
+// $holesCount = 32;
+// $productsManufacturerYears = 2021;
 
-$productPrice = 67.99;
-$size = 41.5;
-$weight = 0.234;
+// $productPrice = 67.99;
+// $size = 41.5;
+// $weight = 0.234;
 
-$isInStock = true;
-$waterProof = true;
-$airless = false;
-echo "<pre>";
-$product = [
-    'name' => $productName, 
-    'qty' => $productQty, 
-    'price' => $productPrice, 
-    'water_proof' => $waterProof
-];
+// $isInStock = true;
+// $waterProof = true;
+// $airless = false;
+// echo "<pre>";
+// $product = [
+//     'name' => $productName, 
+//     'qty' => $productQty, 
+//     'price' => $productPrice, 
+//     'water_proof' => $waterProof
+// ];
 
-$product2 = [
-    'name' => 'Vasariniai Batai Balti', 
-    'qty' => 1, 
-    'price' => 87.99, 
-    'water_proof' => true
-];
-$products = [$product, $product2];
+// $product2 = [
+//     'name' => 'Vasariniai Batai Balti', 
+//     'qty' => 1, 
+//     'price' => 87.99, 
+//     'water_proof' => true
+// ];
+// $products = [$product, $product2];
 
 
 // echo $poduct['name'];
@@ -68,13 +68,21 @@ Eilutes
 
 // = + - / * %;
 
-$pvm = 23;
+// $pvm = 23;
 
-$priceWithTax = $productPrice * 1+$pvm / 100;
+// $priceWithTax = $productPrice * 1+$pvm / 100;
 
-//echo $priceWithTax;
-
-
+// echo $priceWithTax;
 
 
 
+
+
+$title = 'Jusu kazkokai tai antraste';
+$slug = getSlug($title);
+
+echo $slug;
+
+$email = 'Arnoldas@emotion.lt ';
+
+echo clearEmail($email);
