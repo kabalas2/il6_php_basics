41fffbc65947a9847a377e59da492ad80fa1583f
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-01-19 22:22:37 +0200

    changai

--- gumtree/parts/header.php
+++ gumtree/parts/header.php
@@ -7,6 +7,7 @@
     <ul class="menu">
         <li><a href="create.php">Add Ad!</a></li>
         <li><a href="catalog.php">All ads!</a></li>
+        <li><a href="registration.php">New User</a></li>
     </ul>
 </div>
 <div class="content">
--- gumtree/registration.php
+++ gumtree/registration.php
@@ -7,7 +7,8 @@ $password = "root";
 $dbName = 'shop_lt';
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=".$dbName, $username, $password);
+    //mysql:host=localhost;dbname=shop_lt
+    $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch (PDOException $e) {
