6fa06032deb8a0760723f318d16cf001973e35f2
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-01-26 19:09:40 +0200

    composer init

--- oop/index.php
+++ oop/index.php
@@ -1,14 +1,22 @@
 <?php
 
+include 'vendor/autoload.php';
+
 if(isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO'] !== '/'){
     $path = trim($_SERVER['PATH_INFO'],'/');
-
+    echo '<pre>';
     $path = explode('/',$path);
+    print_r($path);
     $class = ucfirst($path[0]);
     $method = $path[1];
     $param = $path[2];
-    include 'app/code/Controller/'.$class.'.php';
+    //echo 'app/code/Controller/'.$class.'.php';
+
+//    $obj = new $class();
+    $class = '\Controller\\'.$class;
     $obj = new $class();
+
+
     $obj->$method($param);
 
 
