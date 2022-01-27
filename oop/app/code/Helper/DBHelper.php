b9a7adba1e0c288e2e871f35af44405ff7621d48
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-01-27 22:00:13 +0200

    dbhelper

---
+++ oop/app/code/Helper/DBHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Helper;
+
+class DBHelper
+{
+    private $conn;
+
+    private $sql;
+
+    public function __construct()
+    {
+        $this->sql = '';
+
+        try {
+            $this->conn = new PDO("mysql:host=".SERVERNAME.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "Connected successfully";
+        } catch (PDOException $e) {
+            //echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public function select($fields = '*')
+    {
+        $this->sql .= 'SELECT '.$fields.' ';
+        return $this;
+    }
+
+    public function from($table)
+    {
+        $this->sql.= ' FROM '.$table.' ';
+        return $this;
+    }
+
+    public function where($field, $value, $operator  = '=')
+    {
+        $this->sql .= ' WHERE '.$field.$operator.'"'.$value.'"';
+        return $this;
+    }
+
+    public function get(){
+        $rez = $this->conn->query($this->sql);
+        return $rez->fetchAll();
+    }
+
+    public function getOne(){
+        $rez = $this->conn->query($this->sql);
+        $data = $rez->fetchAll();
+        return $data[0];
+    }
+}
--- oop/config.php
+++ oop/config.php
@@ -1,3 +1,7 @@
 <?php
 
 define('BASE_URL', 'http://127.0.0.1:8000/index.php/');
+define('SERVERNAME','localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'shop_lt');
