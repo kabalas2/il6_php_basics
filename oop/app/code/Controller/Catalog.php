38d5d8513a5b3f71fb46b1ac34618110e0b03cee
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-02-07 22:23:47 +0200

    catalog model/controller

--- oop/app/code/Controller/Catalog.php
+++ oop/app/code/Controller/Catalog.php
@@ -3,29 +3,121 @@
 namespace Controller;
 
 use Core\AbstractController;
+use Helper\FormHelper;
+use Helper\Url;
+use Model\Ad;
 
 class Catalog extends AbstractController
 {
-    public function show($id = null)
+    public function add()
     {
-        if ($id !== null) {
-            echo 'Catalog controller ID ' . $id;
-        }
-    }
 
-    public function all()
-    {
-        $this->render('catalog/all');
+        if (!isset($_SESSION['user_id'])) {
+            Url::redirect('');
+        }
+        $form = new FormHelper('catalog/create', 'POST');
+        $form->input([
+            'name' => 'title',
+            'type' => 'text',
+            'placeholder' => 'Pavadinimas'
+        ]);
+
+        $form->textArea('description', 'Aprasymas');
+        $form->input([
+            'name' => 'price',
+            'type' => 'text',
+            'placeholder' => 'Kaina'
+        ]);
+        $form->input([
+            'name' => 'year',
+            'type' => 'text',
+            'placeholder' => 'Metai'
+        ]);
+
+        $form->input([
+            'type' => 'submit',
+            'value' => 'sukurti',
+            'name' => 'create'
+        ]);
+
+        $this->data['form'] = $form->getForm();
+        $this->render('catalog/create');
+
     }
 
     public function create()
     {
-
+        $ad = new Ad();
+        $ad->setTitle($_POST['title']);
+        $ad->setDescription($_POST['description']);
+        $ad->setManufacturerId(1);
+        $ad->setModelId(1);
+        $ad->setPrice($_POST['price']);
+        $ad->setYear($_POST['year']);
+        $ad->setTypeId(1);
+        $ad->setUserId($_SESSION['user_id']);
+        $ad->save();
     }
 
-    public function update($data)
+    public function edit($id)
     {
-        echo 'I\'m Robot';
+        $ad = new Ad();
+        $ad->load($id);
+
+        $form = new FormHelper('catalog/update', 'POST');
+        $form->input([
+            'name' => 'title',
+            'type' => 'text',
+            'placeholder' => 'Pavadinimas',
+            'value' => $ad->getTitle()
+        ]);
+
+        $form->input([
+            'name' => 'id',
+            'type' => 'hiden',
+            'value' => $ad->getId()
+
+        ]);
+
+        $form->textArea('description', $ad->getDescription());
+        $form->input([
+            'name' => 'price',
+            'type' => 'text',
+            'placeholder' => 'Kaina',
+            'value' => $ad->getPrice()
+        ]);
+        $form->input([
+            'name' => 'year',
+            'type' => 'text',
+            'placeholder' => 'Metai',
+            'value' => $ad->getYear()
+        ]);
+
+        $form->input([
+            'type' => 'submit',
+            'value' => 'sukurti',
+            'name' => 'create'
+        ]);
+
+        $this->data['form'] = $form->getForm();
+        $this->render('catalog/create');
     }
+
+    public function update()
+    {
+        $adId = $_POST['id'];
+        $ad = new Ad();
+        $ad->load($adId);
+        $ad->setTitle($_POST['title']);
+        $ad->setDescription($_POST['description']);
+        $ad->setManufacturerId(1);
+        $ad->setModelId(1);
+        $ad->setPrice($_POST['price']);
+        $ad->setYear($_POST['year']);
+        $ad->setTypeId(1);
+        $ad->setUserId($_SESSION['user_id']);
+        $ad->save();
+    }
+
 }
 
---
+++ oop/app/code/Model/Ad.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace Model;
+
+use Helper\DBHelper;
+
+class Ad
+{
+    private $id;
+
+    private $title;
+
+    private $description;
+
+    private $manufacturerId;
+
+    private $modelId;
+
+    private $price;
+
+    private $year;
+
+    private $typeId;
+
+    private $userId;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getManufacturerId()
+    {
+        return $this->manufacturerId;
+    }
+
+    /**
+     * @param mixed $manufacturerId
+     */
+    public function setManufacturerId($manufacturerId)
+    {
+        $this->manufacturerId = $manufacturerId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModelId()
+    {
+        return $this->modelId;
+    }
+
+    /**
+     * @param mixed $modelId
+     */
+    public function setModelId($modelId)
+    {
+        $this->modelId = $modelId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param mixed $year
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeId()
+    {
+        return $this->typeId;
+    }
+
+    /**
+     * @param mixed $typeId
+     */
+    public function setTypeId($typeId)
+    {
+        $this->typeId = $typeId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+
+    public function save()
+    {
+        if(isset($this->id)){
+            $this->update();
+        }else{
+            $this->create();
+        }
+    }
+
+    public function update()
+    {
+        $db = new DBHelper();
+        $data = [
+            'title' => $this->title,
+            'description' => $this->description,
+            'manufacturer_id' => $this->manufacturerId,
+            'model_id' => $this->modelId,
+            'price' => $this->price,
+            'year' => $this->year,
+            'type_id' => $this->typeId,
+            'user_id' => $this->userId,
+        ];
+
+        $db->update('ads', $data)->where('id', $this->id)->exec();
+    }
+
+    public function create()
+    {
+        $db = new DBHelper();
+        $data = [
+            'title' => $this->title,
+            'description' => $this->description,
+            'manufacturer_id' => $this->manufacturerId,
+            'model_id' => $this->modelId,
+            'price' => $this->price,
+            'year' => $this->year,
+            'type_id' => $this->typeId,
+            'user_id' => $this->userId,
+        ];
+        $db->insert('ads', $data)->exec();
+    }
+
+    public function load($id)
+    {
+        $db = new DBHelper();
+        $ad = $db->select()->from('ads')->where('id', $id)->getOne();
+        if(!empty($ad)){
+            $this->id = $ad['id'];
+            $this->title = $ad['title'];
+            $this->manufacturerId = $ad['manufacturer_id'];
+            $this->modelId = $ad['mdoel_id'];
+            $this->price = $ad['price'];
+            $this->year = $ad['year'];
+            $this->typeId = $ad['type_id'];
+            $this->userId = $ad['user_id'];
+        }
+
+        return $this;
+    }
+}
