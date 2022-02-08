077774ec895dc9ee9fb78681936516dadcb13252
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-02-08 22:28:24 +0200

    code styles

--- oop/app/design/parts/header.php
+++ oop/app/design/parts/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <title>Autopliusas</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL_WITHOUT_INDEX_PHP.'css/style.css'; ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL_WITHOUT_INDEX_PHP . 'css/style.css'; ?>">
 </head>
 <body>
 <header>
@@ -13,7 +13,7 @@
             <li>
                 <a href="<?php echo BASE_URL ?>/catalog/all">All ads</a>
             </li>
-            <?php if($this->isUserLoged()): ?>
+            <?php if ($this->isUserLoged()): ?>
                 <li>
                     <a href="<?php echo BASE_URL ?>/catalog/add">Add New</a>
                 </li>
@@ -21,12 +21,12 @@
                     <a href="<?php echo BASE_URL ?>/user/logout">Logout</a>
                 </li>
             <?php else: ?>
-            <li>
-                <a href="<?php echo BASE_URL ?>/user/login">Login</a>
-            </li>
-            <li>
-                <a href="<?php echo BASE_URL ?>/user/register">Sign Up</a>
-            </li>
+                <li>
+                    <a href="<?php echo BASE_URL ?>/user/login">Login</a>
+                </li>
+                <li>
+                    <a href="<?php echo BASE_URL ?>/user/register">Sign Up</a>
+                </li>
             <?php endif; ?>
         </ul>
     </nav>
