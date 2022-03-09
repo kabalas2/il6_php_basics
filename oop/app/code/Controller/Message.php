f9e464b3e2e8dbb0c4b936d130e34d626f093532
Arnoldas Turulis <arnoldas.t@emotion.lt>
2022-03-09 19:56:18 +0200

    meesages group simple example

--- oop/app/code/Controller/Message.php
+++ oop/app/code/Controller/Message.php
@@ -23,24 +23,18 @@ class Message extends AbstractController implements ControllerInterface
         $chats = [];
 
         foreach ($messages as $message) {
-            if ($message->getSenderId() > $message->getReseiverId()) {
-                $key = $message->getReseiverId() . '-' . $message->getSenderId();
-            } else {
-                $key = $message->getSenderId() . '-' . $message->getReseiverId();
-            }
+//            if ($message->getSenderId() > $message->getReseiverId()) {
+//                $key = $message->getReseiverId() . '-' . $message->getSenderId();
+//            } else {
+//                $key = $message->getSenderId() . '-' . $message->getReseiverId();
+//            }
             $chatFriendId = $message->getSenderId() == $_SESSION['user_id'] ? $message->getReseiverId() : $message->getSenderId();
             $chatFriend = new \Model\User();
             $chatFriend->load($chatFriendId);
-            // $chat['4-10']
-            // $chat['3-4']
-            // $chat['4-11']
-            $chats[$key]['message'] = $message;
-            $chats[$key]['chat_friend'] = $chatFriend;
+            $chats[$chatFriendId]['message'] = $message;
+            $chats[$chatFriendId]['chat_friend'] = $chatFriend;
         }
-
         $this->data['chat'] = $chats;
-
-
         $this->render('messages/all');
     }
 }
